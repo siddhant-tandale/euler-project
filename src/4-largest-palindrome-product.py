44b8d54834b13a0b49e9f0e87261440ae35208f2
siddhant-tandale <siddhanttandale@gmail.com>
2022-10-13 22:06:50 -0700

    finished 4

---
+++ src/4-largest-palindrome-product.py
@@ -0,0 +1,17 @@
+palin_num = 0
+for number1 in range(999, 100, -1):
+
+    for number2 in range(number1, 100, -1):
+        product = number1*number2
+        prod_str = str(product) 
+        
+        palindrome = True
+        for i in range(len(prod_str)):
+            if prod_str[i] != prod_str[-(i+1)]:
+                palindrome = False
+                break
+
+        if palindrome and product > palin_num:
+            palin_num = product
+        
+print(palin_num)
